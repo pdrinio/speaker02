7a32c414d48cae6014b5dc117bbd1d882c60816b
Pedro Orgeira <poc.desarrollo@hotmail.com>
2018-01-31 18:27:21 +0100

    Esqueleto

---
+++ speaker02/MiControl.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// La plantilla de elemento Control de usuario está documentada en https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace speaker02
+{
+    public sealed partial class MiControl : UserControl
+    {
+        public MiControl()
+        {
+            this.InitializeComponent();
+
+            DataContext = new MiControlViewModel(mediaElement);
+        }
+    }
+}
